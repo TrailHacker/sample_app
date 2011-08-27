dd4bf00ddf5922599ac7755c4920298243fab906
TrailHacker <lucas@hardbarger.com>
2011-08-27 04:36:06 -0400

    Routings work now

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 SampleApp::Application.routes.draw do
+
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
   root :to => 'pages#home'
-
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
