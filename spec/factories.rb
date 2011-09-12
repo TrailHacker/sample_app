105251c705ee25ed9b390c04364533e71e6ee35e
TrailHacker <lucas@hardbarger.com>
2011-09-11 23:58:25 -0400

    User model with passwords, and user show page

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+#by using the symbol ':user', we get Facotry Girl to simulate the User model.
+Factory.define :user do |user|
+  user.name "Luc"
+  user.email "lucas@hardbarger.com"
+  user.password "secret"
+  user.password_confirmation "secret"
+end
