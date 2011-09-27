40eb956d72ba14e110f877b21bd243b32f217e7a
TrailHacker <lucas@hardbarger.com>
2011-09-27 00:22:51 -0400

    Done with sign in

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,13 +2,13 @@ module ApplicationHelper
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
     if @title.nil?
-	    base_title
+      base_title
     else
-	    "#{base_title} | #{@title}"
+      "#{base_title} | #{@title}"
     end
   end
   def logo
-	image_tag("logo.png", :alt => "Sample App", :class => "round") 
+    image_tag("logo.png", :alt => "Sample App", :class => "round") 
   end
 
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -39,14 +39,30 @@ describe "Users" do
     end
   end
 
-#   describe "GET /users" do
+  describe "sign in/out" do
 
-#     it "works! (now write some real specs)" do
-#       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-#       get users_index_path
-#       response.status.should be(200)
-#     end
+    describe "failure" do
 
-#   end
+      it "should not sign a user in" do
+        visit signin_path
+        fill_in :email, :with => ""
+        fill_in :password, :with => ""
+        click_button
+        response.should have_selector("div.flash.error", :content => "Invalid")
+      end
+    end
 
+    describe "success" do
+      it "should sign a user in and out" do
+        user = Factory(:user)
+        visit signin_path
+        fill_in :email, :with => user.email
+        fill_in :password, :with => user.password
+        click_button
+        controller.should be_signed_in
+        click_link "Sign out"
+        controller.should_not be_signed_in
+      end
+    end
+  end
 end
